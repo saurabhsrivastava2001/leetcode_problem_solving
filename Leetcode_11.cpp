26c21b0f8f125aa445cdd9b95b4d020347bdde2f
saurabh srivastava <121008517+saurabhsrivastava2001@users.noreply.github.com>
2023-03-15 13:42:10 +0530

    Add files via upload
    this code is only the main solution class ( according two the format of LeetCode)

---
+++ Leetcode_11.cpp
@@ -0,0 +1,20 @@
+//this is the main solution function required , problem-11 (container with most water)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+       int l=0;
+       int r= ((height.size())-1);
+       int maxx=0;
+       while (l<r){
+           int lh=height[l];
+           int rh=height[r];
+           int min_h= min(rh,lh);
+           int len=r-l;
+           int curr_area=len*min_h;
+           maxx= max(maxx,curr_area);
+           if(lh<rh)l++;
+           else r--; 
+       } 
+       return maxx;
+    }
+};
