209d45272826686caa7cc91d02bc120d712b9b1d
saurabh srivastava <121008517+saurabhsrivastava2001@users.noreply.github.com>
2023-03-30 08:19:03 +0530

    Add files via upload

---
+++ leetcode_744.java
@@ -0,0 +1,14 @@
+//leetcode 744
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int s=0;
+        int e= letters.length -1;
+        while (s<=e){
+            int mid= s+(e-s)/2;
+            if(target<letters[mid]){
+                e=mid-1;
+            }else s=mid+1;
+        }
+        return letters[s%letters.length];
+    }
+}
