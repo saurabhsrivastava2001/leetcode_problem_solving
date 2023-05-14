05efc2e347f9dfcd521eb0f07ef2dac6281ad25f
saurabh srivastava <121008517+saurabhsrivastava2001@users.noreply.github.com>
2023-05-14 20:15:38 +0530

    Update leetcode_34.java

--- leetcode_34.java
+++ leetcode_34.java
@@ -1,4 +1,9 @@
-//leetcode_34 in java
+//leetcode_34 in java  ----------------problem statement---------------
+//Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+//If target is not found in the array, return [-1, -1].
+//You must write an algorithm with O(log n) runtime complexity.
+
+ 
 class Solution {
     public int[] searchRange(int[] nums, int target) {
                 return new int[]{findFirstOccurrence(nums, target), findLastOccurrence(nums, target)};
