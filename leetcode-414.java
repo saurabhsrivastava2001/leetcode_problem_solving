3f3725df11041ff7e1814781a510c0c328afa0ad
saurabh srivastava <121008517+saurabhsrivastava2001@users.noreply.github.com>
2023-05-26 20:24:22 +0530

    Add files via upload

---
+++ leetcode-414.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        Integer max = null;
+        Integer second_max=null;
+        Integer third_max=null;
+        for (Integer num:nums){
+            if (num.equals(max)|| num.equals(second_max)|| num.equals(third_max)){
+                continue;   
+            } 
+            if (max==null || num>max){
+                third_max = second_max;
+                second_max = max;
+                max=num;
+            }else if(second_max==null || num>second_max){
+                third_max = second_max;
+                second_max = num;
+            } else if ( third_max ==null || num>third_max){
+                third_max = num;
+            }
+        }
+        if (third_max==null){
+            return max;
+        }
+        return third_max;
+    }    
+}
